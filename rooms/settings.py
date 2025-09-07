ee660815801a9281dcfa77cf707093d160de52de
javid shabin <javid.main.act@gmail.com>
2025-09-07 09:50:03 +0530

    Remove the pillow configs

--- rooms/settings.py
+++ rooms/settings.py
@@ -2,7 +2,6 @@
 
 from pathlib import Path
 from datetime import timedelta
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,9 +79,6 @@ DATABASES ={
 
 }
 }
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- users/models.py
+++ users/models.py
@@ -13,11 +13,6 @@ class User(AbstractUser):
     is_customer = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    profile_image = models.ImageField(upload_to='profile_images/',
-    blank=True,
-    null=True,
-    max_length=300,  # Increased from default 100
-    default='https://img.freepik.com/premium-vector/avatar-profile-picture-icon-gray-background-flat-design-style-resources-graphic-element-design_991720-445.jpg?semt=ais_incoming&w=740&q=80')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
