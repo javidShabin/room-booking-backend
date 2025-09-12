43b167e6e265fa9271ff54c85de41d39b66792d2
javid shabin <javid.main.act@gmail.com>
2025-09-12 08:49:16 +0530

    Update the urls.py

--- api/v1/hotel/urls.py
+++ api/v1/hotel/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-from api.v1.customer import views
 
-app_name = "customer"
+from api.v1.hotel import views
+
+app_name = "hotel"
 
 urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("signup/", views.signup, name="signup"),
-    path("logout/", views.logout, name="logout"),
-    path("users/", views.get_users_list, name="users_list"),
-    path("user/<int:user_id>/", views.get_user_profile, name="user_profile"),
-    path(
-        "user/profile/update/<int:user_id>/",
-        views.update_user_profile,
-        name="update_user_profile",
-    ),
+    
+  
 ]
--- rooms/urls.py
+++ rooms/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/customer/',include("api.v1.customer.urls",namespace='customer')),
+    path('api/v1/hotel/',include("api.v1.hotel.urls", namespace='hotel'))
 ]
 
 if settings.DEBUG:
