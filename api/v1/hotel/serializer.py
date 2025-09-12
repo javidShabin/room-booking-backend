80850fa883f442954fb4f323eade77526513b0b7
javid shabin <javid.main.act@gmail.com>
2025-09-12 17:31:40 +0530

    Create the serilizer for the hotel

---
+++ api/v1/hotel/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from hotel.models import Hotel
+
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = [
+            'id',
+            'name',
+            'location',
+            'address',
+            'description',
+            'rating',
+            'number_of_rooms',
+            'phone',
+            'email',
+            'website',
+            'is_active',
+            'main_image',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
--- api/v1/hotel/views.py
+++ api/v1/hotel/views.py
@@ -1,3 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import get_object_or_404
+from rest_framework.parsers import MultiPartParser, FormParser
