de16756dc4e024cb0180eae7932c9de7a7c0bfcf
javid shabin <javid.main.act@gmail.com>
2025-09-12 23:55:07 +0530

    Create the hotel acitvate function and set the url for that

--- api/v1/hotel/urls.py
+++ api/v1/hotel/urls.py
@@ -7,6 +7,7 @@ app_name = "hotel"
 urlpatterns = [
     path("create/", views.create_hotel, name="create_hotel"),
     path("hotel_list/", views.get_hotels_list, name="hotel_list"),
-    path("single_hotel/<int:hotel_id>/", views.single_hotel, name="single_hotel")
+    path("single_hotel/<int:hotel_id>/", views.single_hotel, name="single_hotel"),
+    path("activate/<int:hotel_id>/", views.activate_hotel, name="activate_hotel"),
   
 ]
--- api/v1/hotel/views.py
+++ api/v1/hotel/views.py
@@ -1,14 +1,18 @@
+from urllib import response
 from django.core.serializers import serialize
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import MultiPartParser, FormParser
+
+from api.v1 import hotel
 from .serializer import HotelSerializer
 from hotel.models import Hotel
 
 # **************************************************************
 # **************** CURD operations of hotel ********************
+
 
 # Create Hotel
 # ************************
@@ -20,8 +24,14 @@ def create_hotel(request):
     location = request.data.get("location")
     manager_id = request.data.get("manager_id") or request.data.get("managerId")
     if not manager_id:
-        return Response({"status": 6001, "message": "manager_id is required"}, status=400)
-    if name and location and Hotel.objects.filter(name=name, location=location).exists():
+        return Response(
+            {"status": 6001, "message": "manager_id is required"}, status=400
+        )
+    if (
+        name
+        and location
+        and Hotel.objects.filter(name=name, location=location).exists()
+    ):
         return Response({"status": 6001, "message": "Hotel already exists"}, status=400)
     data = request.data.copy()
     data["manager_id"] = manager_id
@@ -42,15 +52,15 @@ def create_hotel(request):
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_hotels_list(request):
-   hotels = Hotel.objects.all()
-   for user in hotels:
-    serializer = HotelSerializer(hotels, many=True)
-    response_data = {
-        "status_code": 6000,
-        "data": serializer.data,
-        "message": "Hotels fetched successfully",
-    }
-    return Response(response_data, status=200)
+    hotels = Hotel.objects.all()
+    for user in hotels:
+        serializer = HotelSerializer(hotels, many=True)
+        response_data = {
+            "status_code": 6000,
+            "data": serializer.data,
+            "message": "Hotels fetched successfully",
+        }
+        return Response(response_data, status=200)
 
 
 # Get hotel by id
@@ -65,6 +75,29 @@ def single_hotel(request, hotel_id):
         "status_code": 6000,
         "data": serializer.data,
         "message": "Hotel fetched successfully",
+    }
+
+    return Response(response_data, status=200)
+
+
+# ************ Admin can activate the hotels after verification ********
+# **********************************************************************
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])  # User must be authenticated
+def activate_hotel(request, hotel_id):
+    if not (request.user.is_customer or request.user.is_manager):
+        return Response({"status": 6001, "message": "Permission denied"}, status=403)
+
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    hotel.is_active = True
+    hotel.save()
+
+    serializer = HotelSerializer(hotel)
+
+    response_data = {
+        "status_code": 6000,
+        "data": serializer.data,
+        "message": "Hotel activated successfully",
     }
 
     return Response(response_data, status=200)
